ea62f90cc3690ef7c8d542d1bde099ea57345885
AnzhelikaAlexandrovna <114605591+AnzhelikaAlexandrovna@users.noreply.github.com>
2023-10-15 12:55:02 +0300

    init

---
+++ components/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Интернет-магазин товаров для дома</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<header class="site-header">
+      <nav class="site-navigation">
+        <a class="logo-link" href="index.php">
+          <img src="img/logo-full.svg" width="62" height="17" alt="Логотип магазина gloevk">
+        </a>
+        <ul class="navigation-list">
+          <li><a href="">Каталог</a></li>
+          <li><a href="">Доставка</a></li>
+          <li><a href="">Контакты</a></li>
+          <li><a href="">Блог</a></li>
+        </ul>
+      </nav>
+    </header>
+</body>
+</html>
